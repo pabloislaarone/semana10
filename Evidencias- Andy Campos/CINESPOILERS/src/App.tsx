5ae8fddd774bcb637891c80ca874f6cfecea059c
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-05-24 13:33:03 -0500

    Evidencias: Andy Campos

---
+++ Evidencias- Andy Campos/CINESPOILERS/src/App.tsx
@@ -0,0 +1,13 @@
+import Card from './components/Card'
+
+function App() {
+  return (
+    <main>
+      <h1>CineSpoilerS 🎬</h1>
+      <Card title="Andy Campos" description="Backend Developer" />
+      <Card title="CineSpoilerS" description="Compra tus tickets sin salir de casa" />
+    </main>
+  )
+}
+
+export default App
